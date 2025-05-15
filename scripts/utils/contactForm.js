1652f0eb2594bf02e9f97afb9f0076a01ddfc45a
Simon B <simon.blecot@wifirst.fr>
2025-05-15 16:06:50 +0200

    Start Modal - nom, prenom, email, telephone - regex

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -8,13 +8,47 @@ function closeModal() {
     modal.style.display = "none";
 }
 
-// // DOM Elements
-// const closeBtn = document.querySelector(".close"); // Séléctionne le bouton qui ferme la modale
+const prenom = getElementById("first");
+const nom = document.getElementById("last"); // Champ nom
+const mail = document.getElementById("email"); // Champ mail
 
-// // launch modal event
-// modalBtns.forEach(btn => { // Parcourt chaque élément "btn"
-//   btn.addEventListener("click", displayModal);
-// });
+function validate() {
+  let isValid = true;
 
-// // close modal event
-// closeBtn.addEventListener("click", closeModal); // Ajoute un écouteur d'évènement 'click' au bouton de fermeture de la modale
+  // Prenom
+  const nameRegex = /^([a-zA-ZÀ-ÖØ-öø-ÿ]{2,})$/; // Création d'une regex pour vérifier que le prénom contient uniquement des lettres en majuscules et minuscules et a une longueur minimale de 2 caractères
+  if (!nameRegex.test(prenom.value)) { // Vérifie si le prénom ne correspond pas à la regex
+    displayError(prenom, "Le prénom doit contenir au moins 2 caractères"); // Affcihe ce message d'erreur si la validation échoue
+    isValid = false; // Marque le formulaire comme invalide
+  }
+
+  // Nom
+  if (!nameRegex.test(nom.value)) { // Regex identique àcelle du prénom
+    displayError(nom, "Le nom doit contenir au moins 2 caractères"); // Affcihe ce message d'erreur si la validation échoue
+    isValid = false; // Marque le formulaire comme invalide
+  }
+
+  // Email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regex qui vérifie le format d'une adresse mail
+  if (!emailRegex.test(mail.value)) { // Vérifie si l'email ne correspond pas au format attendu
+    displayError(mail, "Veuillez entrer une adresse mail valide"); // Affiche ce message d'erreur si la validation échoue
+    isValid = false;  // Marque le formulaire comme invalide
+  }
+
+  // Ici, ça veut dire que le formulaire est bon, donc gérer le message de confirmation avec son apparition et sa disparition
+  if (isValid === true) { // Si toutes les validations sont ok
+    showConfirmation(); // Affiche le message de confirmation
+    return true // Retourne true pour indiquer que le formulaire est valide
+  }
+
+  return isValid; // Retourne le statue de validation (true ou false)
+
+}
+
+document.querySelector("").addEventListener("submit", function(event) { // Ajout d'un écouteur d'événement sur le formulaire lors de sa soumission
+  if (!validate()) { // Vérifie la validation du formulaire
+    event.preventDefault(); // Empêche l'envoi si le formulaire est invalide
+  } else {
+    event.preventDefault(); // Empêche la soumission réelle pour la démonstration
+    showConfirmation(); // Affiche le message de confirmation
+  }});
